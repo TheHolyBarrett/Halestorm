39353ddd7ca4151b936eafa3c8c8614dc4e3aa3a
TheHolyBarrett <william_barrett17@milton.edu>
2016-06-28 10:12:53 -0400

    6/28/16 Prelim UI Barrett

--- MainActivity.java
+++ MainActivity.java
@@ -1,71 +1,42 @@
-package edu.milton.test4;
+package edu.milton.myapplication;
 
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.*;
 
-import android.app.ProgressDialog;
-import android.content.Context;
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.os.AsyncTask;
-import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.ProgressBar;
-import android.widget.RatingBar;
-import android.widget.TextView;
-import android.widget.Toast;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
-//import com.google.gson.Gson;
-/*import com.hisham.jsonparsingdemo.models.MovieModel;
-import com.nostra13.universalimageloader.core.DisplayImageOptions;
-import com.nostra13.universalimageloader.core.ImageLoader;
-import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
-import com.nostra13.universalimageloader.core.assist.FailReason;
-import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
-*/
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import java.util.*;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-        new JSONTask().execute();
-
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        assert fab != null;
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -74,21 +45,98 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Button button = (Button)findViewById(R.id.button);
-        button.setOnClickListener(
-                new Button.OnClickListener() {
-                    public void onClick(View v) {
-                        TextView textView= (TextView) findViewById(R.id.textView);
-                        textView.setText("Clicked");
+        /*Button hereButton = (Button)findViewById(R.id.hereButton);
+
+        assert hereButton != null;
+        hereButton.setOnClickListener(
+            new Button.OnClickListener(){
+                public void onClick(View v){
+                    TextView text = (TextView)findViewById(R.id.textView);
+                    text.setText("Thank you for checking in");
+                }
+            }
+        );*/
+        Button checkInButton = (Button)findViewById(R.id.checkInButton);
+        checkInButton.setVisibility(View.INVISIBLE);
+        String[] list = {"Bob", "Joe", "Fred", "Harry", "Cads", "WesterG", "Other Bob", "William Manchester Barrett III"};
+        ListAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list);
+        ListView listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(adapter);
+        listView.setVisibility(View.INVISIBLE);
+        String[] tabs = {"Check in", "Emergency"};
+        ListAdapter adapterTabs = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, tabs);
+        ListView tabList = (ListView) findViewById(R.id.listView2);
+        tabList.setAdapter(adapterTabs);
+        TextView text = (TextView)findViewById(R.id.checkInText);
+        text.setText("");
+
+        tabList.setOnItemClickListener(
+                new AdapterView.OnItemClickListener(){
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        String name =  String.valueOf(parent.getItemAtPosition(position));
+                        ListView tabList = (ListView) findViewById(R.id.listView2);
+                        tabList.setVisibility(View.INVISIBLE);
+                        if(name.equals("Check in")) {
+                        ListView listView = (ListView) findViewById(R.id.listView);
+                        listView.setVisibility(View.VISIBLE);
+                            TextView text = (TextView) findViewById(R.id.checkInText);
+                            text.setText("Please Check In");
+                        }
+                        if(name.equals("Emergency")){
+                            TextView text = (TextView) findViewById(R.id.checkInText);
+                            text.setText("Are you in immediate danger?");
+
+                        }
+
+
 
                     }
                 }
         );
+        listView.setOnItemClickListener(
+                new AdapterView.OnItemClickListener() {
+
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                        String name =  String.valueOf(parent.getItemAtPosition(position));
+                        TextView text = (TextView)findViewById(R.id.checkInText);
+                        text.setText(""+name);
+                        ListView listView = (ListView) findViewById(R.id.listView);
+                        listView.setVisibility(View.INVISIBLE);
+                        Button checkInButton = (Button)findViewById(R.id.checkInButton);
+                        checkInButton.setVisibility(View.VISIBLE);
+                        checkInButton.setOnClickListener(
+                                new Button.OnClickListener() {
+                                    public void onClick(View v) {
+                                        Button checkInButton = (Button) findViewById(R.id.checkInButton);
+                                        checkInButton.setVisibility(View.INVISIBLE);
+                                        TextView text = (TextView) findViewById(R.id.checkInText);
+                                        text.setText("Thank you");
 
 
 
+                                    }
+                                }
+
+                                    );
 
 
+
+                        //Toast.makeText(MainActivity.this, name, Toast.LENGTH_LONG).show();
+                                }
+                    public void onItemClick( View v)
+                    {
+
+                    }
+
+
+                }
+        );
+
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     @Override
@@ -96,14 +144,10 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
-
     }
-
-
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        System.out.println("Button selected");
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
@@ -116,6 +160,44 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://edu.milton.myapplication/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://edu.milton.myapplication/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
 }
-
-
